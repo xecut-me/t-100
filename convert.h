774387266d9234c6d48fb70a45c06ebb3a5530bb
fhunter <fhunter@fhunter.link>
2025-10-19 17:14:45 +0200

    Added conversion arrays template

---
+++ convert.h
@@ -0,0 +1,15 @@
+#ifndef __CONVERT_H__
+#define __CONVERT_H__
+
+const uint8_t PROGMEM ascii2baudot[128] = 
+{ 
+
+};
+
+
+// lower part - LTRS, upper part - FIGS
+const uint8_t PROGMEM baudot2ascii[64] = 
+{ 0,
+};
+
+#endif //__CONVERT_H__
