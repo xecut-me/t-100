44a75e803bd95ac764fecf14fe2783cfcfa71edb
fhunter <fhunter@fhunter.link>
2025-10-19 02:17:56 +0200

    Added comments and led blinking/init

--- teletype1.ino
+++ teletype1.ino
@@ -1,6 +1,11 @@
 #include <protothreads.h>
 #include <CircularBuffer.hpp>
 
+#define TXPIN   7
+#define RXPIN   6
+#define HALF_BIT    10
+const char PROGMEM LEDS[4]={A0,A1,A2,A3};
+
 pt ptTXThread;
 pt ptRXThread;
 CircularBuffer<char, 16> queue;
@@ -13,19 +18,21 @@ int rxThread(struct pt* pt) {
 
   // Loop forever
   for(;;) {
-  // read the state of the pushbutton value:
-    PT_WAIT_UNTIL(pt, digitalRead(6) == LOW);
-    PT_SLEEP(pt, 10);
+    PT_WAIT_UNTIL(pt, digitalRead(RXPIN) == HIGH);// wait for --+       to indicate start bit (this pair prevents freerunning zeroes reading on broken loop)
+    PT_WAIT_UNTIL(pt, digitalRead(RXPIN) == LOW); //            +----
+    digitalWrite(LEDS[0], HIGH);
+    PT_SLEEP(pt, HALF_BIT);                       // sleep till the middle of start bit (10 ms)
     x = 0;
     for(i=0;i<5;i++){
-      PT_SLEEP(pt, 20);
-      if(digitalRead(6) == HIGH) { // 1
-        x = x | 0x01;
-      }
-      x = x << 1;
+      PT_SLEEP(pt, HALF_BIT*2);                   // wait till next bit
+      if(digitalRead(RXPIN) == HIGH) {            // 1  ---+  +--+--+--+--+--+---   
+        x = x | 0x01;                             //       |S | 0| 1| 2| 3| 4|STP    - bits are 20ms, stop bit - 30ms
+      };                                          //       +--+--+--+--+--+--+   
+      x = x << 1;                                 // shift the result
     }
-    PT_SLEEP(pt, 40);
-    Serial.write(x);
+    PT_SLEEP(pt, HALF_BIT*4);                     // 1.5 stop bits ( 0.5 bit forward from last bit + 1.5 bits of stop)
+    Serial.write(x);                              // FIXME: here should be conversion
+    digitalWrite(LEDS[0], LOW);
   }
 
   PT_END(pt);
@@ -39,38 +46,41 @@ int txThread(struct pt* pt) {
 
   // Loop forever
   for(;;) {
-  // read the state of the pushbutton value:
-    PT_WAIT_UNTIL(pt, !queue.isEmpty());
+    PT_WAIT_UNTIL(pt, !queue.isEmpty());        // wait till there is something to send
+    digitalWrite(LEDS[1], HIGH);
     x = queue.pop();
-    digitalWrite(7, LOW);
-    PT_SLEEP(pt, 20);
+    digitalWrite(TXPIN, LOW);                   // send start bit
+    PT_SLEEP(pt, HALF_BIT*2);                   //
     for(i=0;i<5;i++){
       if(x & 0x01) {
-        digitalWrite(7, LOW);
+        digitalWrite(TXPIN, LOW);               // loop control is inverted, low breaks the loop
       }else{
-        digitalWrite(7, HIGH);
+        digitalWrite(TXPIN, HIGH);
       }
-      PT_SLEEP(pt, 20);
+      PT_SLEEP(pt, HALF_BIT*2);
       x = x >> 1;
     }
-    digitalWrite(7, HIGH);
-    PT_SLEEP(pt, 30);
+    digitalWrite(TXPIN, HIGH);
+    digitalWrite(LEDS[1], LOW);
+    PT_SLEEP(pt, HALF_BIT*3);                   // wait 1.5 bits
   }
 
   PT_END(pt);
 }
 
 
-
-
 void setup() {
   // put your setup code here, to run once:
   PT_INIT(&ptRXThread);
   PT_INIT(&ptTXThread);  
-  pinMode(7, OUTPUT);
-  pinMode(6, INPUT);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
+  pinMode(TXPIN, OUTPUT);
+  pinMode(RXPIN, INPUT);
+  digitalWrite(RXPIN, HIGH);
+  digitalWrite(TXPIN, HIGH);
+  for(auto i=0;i<sizeof(LEDS)/sizeof(LEDS[0]);i++){
+      pinMode(LEDS[i], OUTPUT);
+      digitalWrite(LEDS[i], LOW);
+  }
   Serial.begin(9600);
 }
 
@@ -80,7 +90,7 @@ void loop() {
     if(queue.isFull()) {
       Serial.write("Overflow!");
     }else{
-      queue.push(Serial.read());
+      queue.push(Serial.read()); // FIXME: here should be conversion
     }
   }
   PT_SCHEDULE(rxThread(&ptRXThread));
