8a861e5c8b491c5c870713f65f2677a05a60bac7
fhunter <fhunter@fhunter.link>
2025-10-19 04:03:12 +0200

    Fixed polarities and bit order

--- teletype1.ino
+++ teletype1.ino
@@ -24,11 +24,11 @@ int rxThread(struct pt* pt) {
     PT_SLEEP(pt, HALF_BIT);                       // sleep till the middle of start bit (10 ms)
     x = 0;
     for(i=0;i<5;i++){
+      x = x << 1;                                 // shift the result
       PT_SLEEP(pt, HALF_BIT*2);                   // wait till next bit
       if(digitalRead(RXPIN) == HIGH) {            // 1  ---+  +--+--+--+--+--+---   
         x = x | 0x01;                             //       |S | 0| 1| 2| 3| 4|STP    - bits are 20ms, stop bit - 30ms
       };                                          //       +--+--+--+--+--+--+   
-      x = x << 1;                                 // shift the result
     }
     PT_SLEEP(pt, HALF_BIT*4);                     // 1.5 stop bits ( 0.5 bit forward from last bit + 1.5 bits of stop)
     Serial.write(x);                              // FIXME: here should be conversion
@@ -52,13 +52,13 @@ int txThread(struct pt* pt) {
     digitalWrite(TXPIN, LOW);                   // send start bit
     PT_SLEEP(pt, HALF_BIT*2);                   //
     for(i=0;i<5;i++){
-      if(x & 0x01) {
-        digitalWrite(TXPIN, LOW);               // loop control is inverted, low breaks the loop
+      if(x & 0x10) {
+        digitalWrite(TXPIN, HIGH);               // loop control is inverted, low breaks the loop
       }else{
-        digitalWrite(TXPIN, HIGH);
+        digitalWrite(TXPIN, LOW);
       }
       PT_SLEEP(pt, HALF_BIT*2);
-      x = x >> 1;
+      x = x << 1;
     }
     digitalWrite(TXPIN, HIGH);
     digitalWrite(LEDS[1], LOW);
