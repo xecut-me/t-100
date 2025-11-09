43ee905efb3a26cc90a89b9dda4cde51ce6b7141
enovikov11@yandex.ru <Evgenii Novikov>
2025-11-09 23:45:27 +0100

    applied formatting

--- tests/prbs_receive.py
+++ tests/prbs_receive.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-import time
 import sys
-import os
+
 
 def prbs15_gen(seed=1):
     """
@@ -15,25 +14,27 @@ def prbs15_gen(seed=1):
         bits = lfsr & 0x1f  # output least significant bit
         lfsr = ((lfsr << 1) | new_bit) & 0x7FFF  # shift and insert new bit
         yield bits
-        
+
+
 generator = prbs15_gen(seed=0x3)
 
 errors = 0
 received = 0
 
 while True:
-    t1=sys.stdin.buffer.read(1)
+    t1 = sys.stdin.buffer.read(1)
     received = received + 1
     i = t1[0]
-    t2=next(generator)
-    if i!=t2:
+    t2 = next(generator)
+    if i != t2:
         errors = errors + 1
         diff = i ^ t2
         binary_string = format(diff, '08b')
-        t2_bin = format(t2,'08b')
-        i_bin = format(i,'08b')
-        error_rate = 100.0* errors/received
-        print(f"ERROR: wrong bits: {binary_string} expected: {t2_bin} got {i_bin} error rate: {error_rate}")
+        t2_bin = format(t2, '08b')
+        i_bin = format(i, '08b')
+        error_rate = 100.0 * errors/received
+        print(
+            f"ERROR: wrong bits: {binary_string} expected: {t2_bin} got {i_bin} error rate: {error_rate}")
     else:
-        error_rate = 100.0* errors/received
+        error_rate = 100.0 * errors/received
         print(f". error rate: {error_rate}")
