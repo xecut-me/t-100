9a55a483cfab8bbf3af7282435709f7357113de5
fhunter <fhunter@fhunter.link>
2025-11-26 17:48:45 +0100

    Refactored rx/tx section, for use without protothreads

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,7 +2,6 @@
 #include <U8g2lib.h>
 #include <WiFi.h>
 #include <Wire.h>
-#include <protothreads.h>
 
 #include "convert.h"
 #include "credentials.h"
@@ -16,8 +15,8 @@
 //
 // It should:
 //
-//    X - connect to wifi via hardcoded credentials
-//    X - get IP over dhcp
+//    V - connect to wifi via hardcoded credentials
+//    V - get IP over dhcp
 //    X - listen on port 1337 for raw tcp connection
 //    X - when there is no connection - it should enable loopback mode on
 //        teletype (send everything that is typed on keyboard - back to
@@ -38,6 +37,8 @@ devstatus_t current_status;
 #define TXPIN 0
 #define RXPIN 1
 #define HALF_BIT 10
+#define SDAPIN 2
+#define SCLPIN 3
 
 // Delays and width
 #define LINE_WIDTH 69             // FIXME - check if it is correct
@@ -45,131 +46,71 @@ devstatus_t current_status;
 #define LINE_FEED_DELAY 500       // in milliseconds
 //
 
-// if defined - sends data as-is
-// if not defined - converts ascii -> baudot and baudot -> ascii
-// #define IS_RAW
-
-#ifndef IS_RAW
 #include "convert.h"
-bool tx_is_in_ltrs = true;
-bool rx_is_in_ltrs = true;
-bool is_in_loopback = true; // teletype starts in loopback mode till first
-                            // received character from PC
 
-bool rx_allowed = true; // buffer is not full
-#endif
-
-pt ptTXThread;
-pt ptRXThread;
 CircularBuffer<uint8_t, 64> queue;
 
-int rxThread(struct pt *pt) {
-  PT_BEGIN(pt);
-
-  static uint8_t x;
-  static uint8_t i = 0;
-
-  // Loop forever
-  for (;;) {
-    // wait for --+
-    //            +----
-    // to indicate start bit (this pair prevents freerunning zeroes reading on
-    // broken loop)
-    PT_WAIT_UNTIL(pt, digitalRead(RXPIN) == HIGH);
-    PT_WAIT_UNTIL(pt, digitalRead(RXPIN) == LOW);
-    PT_SLEEP(pt, HALF_BIT); // sleep till the middle of start bit (10 ms)
-    x = 0;
-    for (i = 0b000001; i != 0b100000; i <<= 1) {
-      PT_SLEEP(pt, HALF_BIT * 2); // wait till next bit
-      // 1  ---+  +--+--+--+--+--+---
-      //       |S | 0| 1| 2| 3| 4|STP    - bits are 20ms, stop bit - 30 ms
-      //       +--+--+--+--+--+--+
-      if (digitalRead(RXPIN) == HIGH) {
-        x = x | i;
-      };
-    }
-    PT_SLEEP(pt, HALF_BIT * 4); // 1.5 stop bits ( 0.5 bit forward from last bit
-                                // + 1.5 bits of stop)
-
-    if (is_in_loopback) {
-      // while in loopback mode - just write data to the type queue
-      queue.push(x);
-    };
-#ifdef IS_RAW
+void onReceiveTTY() {
+  // process available data from Serial1
+  auto bytes = Serial1.available();
+  uint8_t x = Serial1.read();
+  if (current_status.loopback) {
+    queue.push(x);
+  };
+  switch (current_status.mode) {
+  case MODE_RAW:
     Serial.write(x);
-#else
+    break;
+  case MODE_ASCII:
     if ((x == BAUD_FIGS) || (x == BAUD_LTRS)) {
-      rx_is_in_ltrs = (x == BAUD_LTRS);
+      current_status.rx_is_in_ltrs = (x == BAUD_LTRS);
     } else {
-      x = rx_is_in_ltrs ? (x) : (x | 0x20);
-      Serial.write(pgm_read_byte(baudot2ascii + x));
-    }
-#endif
-  }
-
-  PT_END(pt);
-}
-
-int txThread(struct pt *pt) {
-  PT_BEGIN(pt);
-
-  static char x;
-  static uint8_t i = 0;
-  static uint8_t line_position = 0;
-
-  // Loop forever
-  for (;;) {
-#ifndef IS_RAW
-    if (queue.isEmpty() && !rx_allowed) {
-      Serial.write(XON);
-      rx_allowed = true;
+      x = current_status.rx_is_in_ltrs ? (x) : (x | 0x20);
+      Serial.write(baudot2ascii[x]);
     };
-#endif
-    PT_WAIT_UNTIL(pt, !queue.isEmpty()); // wait till there is something to send
-    x = queue.shift();
-    digitalWrite(TXPIN, LOW);   // send start bit
-    PT_SLEEP(pt, HALF_BIT * 2); //
-    for (i = 0b000001; i != 0b100000; i <<= 1) {
-      digitalWrite(TXPIN, (x & i) ? HIGH : LOW);
-      PT_SLEEP(pt, HALF_BIT * 2);
-    }
-    digitalWrite(TXPIN, HIGH);
-    PT_SLEEP(pt, HALF_BIT * 3); // wait 1.5 bits
-    switch (x) {
-    case BAUD_CR:
-      line_position = 0;
-      PT_SLEEP(pt, CARRIAGE_RETURN_DELAY);
-      break;
-    case BAUD_LF:
-      PT_SLEEP(pt, LINE_FEED_DELAY);
-      break;
-    case BAUD_LTRS:
-    case BAUD_NULL:
-    case BAUD_FIGS:
-      break;
-    default:
-      line_position++;
-    };
-    if (line_position == LINE_WIDTH) {
-      PT_SLEEP(pt, CARRIAGE_RETURN_DELAY);
-      line_position = 0;
-    };
-  }
+    break;
+  case MODE_PUNCH:
+    break; // in punch mode - ignore keyboard
+  default:
+    break;
+  };
+};
 
-  PT_END(pt);
-}
+void handleTTYtx() {
+  // send data via Serial1
+  uint8_t x = queue.shift();
+  Serial1.write(x);
+  switch (x) {
+  case BAUD_CR:
+    current_status.line_position = 0;
+    // PT_SLEEP(pt, CARRIAGE_RETURN_DELAY); // fixme
+    break;
+  case BAUD_LF:
+    // PT_SLEEP(pt, LINE_FEED_DELAY); // fixme
+    break;
+  case BAUD_LTRS:
+  case BAUD_NULL:
+  case BAUD_FIGS:
+    break;
+  default:
+    current_status.line_position++;
+  };
+  if (current_status.line_position == LINE_WIDTH) {
+    // PT_SLEEP(pt, CARRIAGE_RETURN_DELAY); // fixme
+    current_status.line_position = 0;
+  };
+};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial1.begin(50, SERIAL_5N2, 1 /* RX */, 2 /* TX */, false /* invert */);
-  Wire.setPins(2, 3);
+  Serial1.begin(50, SERIAL_5N2, TXPIN /* RX */, RXPIN /* TX */,
+                false /* invert */);
+  Wire.setPins(SDAPIN, SCLPIN);
   Wire.setClock(1000000);
   Wire.begin();
   u8g2.begin();
 
-  PT_INIT(&ptRXThread);
-  PT_INIT(&ptTXThread);
   pinMode(TXPIN, OUTPUT);
   pinMode(RXPIN, INPUT);
   digitalWrite(RXPIN, HIGH);
@@ -180,6 +121,9 @@ void setup() {
   current_status.loopback = true;
   current_status.mode = MODE_ASCII;
   current_status.last_rx = current_status.last_tx = millis();
+  current_status.rx_is_in_ltrs = true;
+  current_status.tx_is_in_ltrs = true;
+  current_status.line_position = 0;
 
   // Connect to WPA2 Wi-Fi network
   WiFi.begin(ssid, password);
@@ -200,100 +144,121 @@ void setup() {
 }
 
 void loop() {
+  // Update status
   current_status.current_ip = WiFi.localIP().toString();
   current_status.wifi_connected = (WiFi.status() == WL_CONNECTED);
+  current_status.rx_count = queue.size();
+  current_status.rx_total = queue.capacity;
+  // End of update status
+  // Redraw display
   u8g2.firstPage();
   do {
-    auto old = millis();
     draw_status(&current_status);
-    Serial.print("Draw took");
-    Serial.print(millis() - old);
-    Serial.println();
   } while (u8g2.nextPage());
-  // put your main code here, to run repeatedly:
+  // End of redraw display
+
+  if (Serial1.available()) {
+    // data from teletype available, process it
+    onReceiveTTY();
+  };
+  if (!queue.isEmpty()) {
+    // there is data in TX fifo, send it
+    handleTTYtx();
+  };
 
   if (Serial.available()) {
     if (queue.size() >= (queue.capacity >> 1)) {
-#ifdef IS_RAW
-      Serial.write("Overflow!");
-#else
-      Serial.write(XOFF);
-      rx_allowed = false;
-#endif // IS_RAW
+      switch (current_status.mode) {
+      case MODE_RAW:
+      case MODE_PUNCH:
+        Serial.write("Overflow!");
+        break;
+      case MODE_ASCII:
+        Serial.write(XOFF);
+        break;
+      default:
+        break;
+      };
+      //  rx_allowed = false; // fixme
     } else {
       uint8_t ascii = Serial.read();
       // drop out of loopback mode on first received character
-      is_in_loopback = false;
-#ifdef IS_RAW
-      queue.push(ascii);
-#else
-      uint8_t baudot;
-      if (ascii > 127) {
-        baudot = 0b00000000; // ignore symbols, with codes above 127
-      } else {
-        baudot = pgm_read_byte(
-            ascii2baudot + ascii); // perform conversion
-                                   // upper bits (765) - mean:
-                                   // 7 - special
-                                   // 6 - can be in letters
-                                   // 5 - can be in figures
-                                   // when both 5 & 6 are zero - ignore symbol
-      };
-      if (baudot & SPECIAL) {
-        // special treatment symbol
-        baudot &= 0x1f;
-        switch (baudot) {
-        case 1: // vertical tab
-          queue.push(BAUD_SPACE);
-          queue.push(BAUD_SPACE);
-          queue.push(BAUD_SPACE);
-          queue.push(BAUD_SPACE);
-          break;
-        case 2: // form feed
-          queue.push(BAUD_LF);
-          queue.push(BAUD_LF);
-          break;
-        case 3: // escape
-          queue.push(BAUD_FIGS);
-          queue.push(BAUD_K_BR_L);
-          queue.push(BAUD_LTRS);
-          queue.push(BAUD_E_3);
-          queue.push(BAUD_S_APO);
-          queue.push(BAUD_C_COLO);
-          queue.push(BAUD_FIGS);
-          queue.push(BAUD_L_BR_R);
-          if (tx_is_in_ltrs) {
+      current_status.loopback = false;
+      switch (current_status.mode) {
+      case MODE_RAW:
+        queue.push(ascii);
+        break;
+      case MODE_ASCII:
+        uint8_t baudot;
+        if (ascii > 127) {
+          baudot = 0b00000000; // ignore symbols, with codes above 127
+        } else {
+          baudot = ascii2baudot[ascii];
+          // perform conversion
+          // upper bits (765) - mean:
+          // 7 - special
+          // 6 - can be in letters
+          // 5 - can be in figures
+          // when both 5 & 6 are zero - ignore symbol
+        };
+        if (baudot & SPECIAL) {
+          // special treatment symbol
+          baudot &= 0x1f;
+          switch (baudot) {
+          case 1: // vertical tab
+            queue.push(BAUD_SPACE);
+            queue.push(BAUD_SPACE);
+            queue.push(BAUD_SPACE);
+            queue.push(BAUD_SPACE);
+            break;
+          case 2: // form feed
+            queue.push(BAUD_LF);
+            queue.push(BAUD_LF);
+            break;
+          case 3: // escape
+            queue.push(BAUD_FIGS);
+            queue.push(BAUD_K_BR_L);
             queue.push(BAUD_LTRS);
+            queue.push(BAUD_E_3);
+            queue.push(BAUD_S_APO);
+            queue.push(BAUD_C_COLO);
+            queue.push(BAUD_FIGS);
+            queue.push(BAUD_L_BR_R);
+            if (current_status.tx_is_in_ltrs) {
+              queue.push(BAUD_LTRS);
+            };
+            break;
+          default:
+            break;
+          }
+        } else {
+          uint8_t flags =
+              (baudot & (CANFIG | CANLTR)) >> 5; // 011_____ -> 0000 0011
+          if (!current_status.tx_is_in_ltrs) {
+            flags |= 0x04;
           };
-          break;
-        default:
-          break;
-        }
-      } else {
-        uint8_t flags =
-            (baudot & (CANFIG | CANLTR)) >> 5; // 011_____ -> 0000 0011
-        if (!tx_is_in_ltrs) {
-          flags |= 0x04;
+          switch (flags) { // !is_in_ltrs | canfig | cantrl
+          case 0b010:      // tx = LTRS, symbol can only be in FIGS - switch
+            queue.push(BAUD_FIGS);
+            current_status.tx_is_in_ltrs = false;
+            break;
+          case 0b101: // tx = FIGS, symbol can only be in LTRS - switch
+            queue.push(BAUD_LTRS);
+            current_status.tx_is_in_ltrs = true;
+            break;
+          }
+          if (flags & ((CANFIG | CANLTR) >> 5)) { // not an ignore symbol
+            queue.push(baudot);
+          };
         };
-        switch (flags) { // !is_in_ltrs | canfig | cantrl
-        case 0b010:      // tx = LTRS, symbol can only be in FIGS - switch
-          queue.push(BAUD_FIGS);
-          tx_is_in_ltrs = false;
-          break;
-        case 0b101: // tx = FIGS, symbol can only be in LTRS - switch
-          queue.push(BAUD_LTRS);
-          tx_is_in_ltrs = true;
-          break;
-        }
-        if (flags & ((CANFIG | CANLTR) >> 5)) { // not an ignore symbol
-          queue.push(baudot);
-        };
-      }
-#endif
+        break;
+      case MODE_PUNCH:
+        break; // FIXME - implement font
+      default:
+        break;
+      };
     }
   }
-  PT_SCHEDULE(rxThread(&ptRXThread));
-  PT_SCHEDULE(txThread(&ptTXThread));
   // Check if a client has connected
   /*  WiFiClient client = server.accept();
     if (client) {
--- arduino/types.hpp
+++ arduino/types.hpp
@@ -8,7 +8,9 @@ typedef enum { MODE_RAW, MODE_ASCII, MODE_PUNCH } teletype_mode_t;
 typedef struct {
   bool wifi_connected;
   String current_ip;
-  bool loopback;
+  bool loopback; // teletype starts in loopback mode till first
+                 // received character from PC
+                 // it returns to loopback as soon as connection is lost
   teletype_mode_t mode;
   unsigned long last_rx;
   bool rx_lit;
@@ -19,6 +21,11 @@ typedef struct {
   uint8_t tx_total;
   uint8_t rx_count;
   uint8_t rx_total;
+
+  bool tx_is_in_ltrs = true;
+  bool rx_is_in_ltrs = true;
+
+  uint8_t line_position; // caret position, to enable delays
 } devstatus_t;
 
 #endif // __TYPES_HPP__
