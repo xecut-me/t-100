7bd9c4d3b05d1fe4a136741d7b71eafc4d6766d2
fhunter <fhunter@fhunter.link>
2026-01-02 18:22:29 +0100

    Preliminarily enabled OTA, removed telnet negotitation

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,4 +1,5 @@
 #include <CircularBuffer.hpp>
+#include <ArduinoOTA.h>
 #include <U8g2lib.h>
 #include <WiFi.h>
 #include <Wire.h>
@@ -31,6 +32,9 @@ devstatus_t current_status;
 
 CircularBuffer<uint8_t, 128> queue;
 
+uint32_t last_ota_time = 0;
+
+
 void WiFiEvent(WiFiEvent_t event) {
   switch (event) {
   case ARDUINO_EVENT_WIFI_STA_CONNECTED:
@@ -44,6 +48,7 @@ void WiFiEvent(WiFiEvent_t event) {
   case ARDUINO_EVENT_WIFI_STA_GOT_IP:
     current_status.current_ip = WiFi.localIP().toString();
     server.begin();
+    ArduinoOTA.begin();
     break;
   case ARDUINO_EVENT_WIFI_STA_LOST_IP:
     current_status.current_ip = "N/A";
@@ -138,6 +143,8 @@ void setup() {
   Serial.begin(115200);
   Serial1.begin(50, SERIAL_5N2, RXPIN /* RX */, TXPIN /* TX */,
                 false /* invert */);
+
+  Serial1.setRxBufferSize(16);
   Wire.setPins(SDAPIN, SCLPIN);
   Wire.setClock(1000000);
   Wire.begin();
@@ -162,6 +169,42 @@ void setup() {
   WiFi.setAutoReconnect(true);
 
   // server.begin();
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      if (millis() - last_ota_time > 500) {
+        Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
+        last_ota_time = millis();
+      }
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
 }
 
 void loop() {
@@ -187,6 +230,7 @@ void loop() {
       handleNetDisconnect();
     }
     currentClient = newClient; // Update with new client connection
+//    currentClient.write("\377\375\042\377\373\001"); // telnet negotiation
   };
   // end handling wifi connection
 
@@ -272,4 +316,5 @@ void loop() {
       };
     }
   }
+  ArduinoOTA.handle();
 }
